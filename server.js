7e6fcddc5ae8e21f214cbd26dd00f97ed2a970be
skassanjee <skassanjee@gmail.com>
2022-03-23 00:45:43 -0600

    sdfsd

--- server.js
+++ server.js
@@ -46,6 +46,8 @@ if (process.env.NODE_ENV === 'production') {
 	  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
   }
+
+  
 app.listen(port, () => {
 	console.log(`server running on port ${port}`)
 })
