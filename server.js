ad4c952b9e475d2e842148deb0217b2f4c0e472f
skassanjee <skassanjee@gmail.com>
2022-03-23 00:36:57 -0600

    sdfvf

--- server.js
+++ server.js
@@ -38,13 +38,14 @@ const port = process.env.PORT || 4000
 
 
 //serve static assets in production
-if(process.env.NODE_ENV === 'production'){
-	app.use(express.static('client/build'))
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+  
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	})
-}
-
+	  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+  }
 app.listen(port, () => {
 	console.log(`server running on port ${port}`)
 })
